d952879cd3ba336d593db6fd134ae277f8c31ca1
tranhoanglon98 <you@example.com>
2022-06-16 20:12:12 +0700

    fix code - clean code

--- src/bai_tap/clean_code_refactoring/TennisGame.java
+++ src/bai_tap/clean_code_refactoring/TennisGame.java
@@ -1,63 +1,78 @@
 package bai_tap.clean_code_refactoring;
 
 public class TennisGame {
+    final static int LOVE = 0;
+    final static int FIFTEEN = 1;
+    final static int THIRTY = 2;
+    final static int FORTY = 3;
+
+    public static String getDeuceScore(int score) {
+        switch (score) {
+            case LOVE:
+                return "Love-All";
+            case FIFTEEN:
+                return "Fifteen-All";
+            case THIRTY:
+                return "Thirty-All";
+
+            case FORTY:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    public static String getFinishScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+        int minusResult = player1Score - player2Score;
+        if (minusResult == 1) {
+            return "Advantage " + player1Name;
+        } else if (minusResult == -1) {
+            return "Advantage " + player2Name;
+        } else if (minusResult >= 2) {
+            return "Win for " + player1Name;
+        } else {
+            return "Win for " + player2Name;
+        }
+    }
+
+    public static String setScore(int player1Score, int player2Score) {
+        int tempScore;
+        StringBuilder score = new StringBuilder();
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = player1Score;
+            else {
+                score.append("-");
+                tempScore = player2Score;
+            }
+            switch (tempScore) {
+                case LOVE:
+                    score.append("Love");
+                    break;
+                case FIFTEEN:
+                    score.append("Fifteen");
+                    break;
+                case THIRTY:
+                    score.append("Thirty");
+                    break;
+                case FORTY:
+                    score.append("Forty");
+                    break;
+            }
+        }
+        return score.toString();
+    }
 
     public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
-        final int love = 0;
-        final int fifteen = 1;
-        final int thirty = 2;
-        final int forty = 3;
-        String score = "";
-        int tempScore;
+        String score;
         boolean isDeuce = player1Score == player2Score;
         if (isDeuce) {
-            switch (player1Score) {
-                case love:
-                    score = "Love-All";
-                    break;
-                case fifteen:
-                    score = "Fifteen-All";
-                    break;
-                case thirty:
-                    score = "Thirty-All";
-                    break;
-                case forty:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (player1Score > forty || player2Score > forty) {
-            int minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = "Advantage " + player1Name;
-            else if (minusResult == -1) score = "Advantage " + player2Name;
-            else if (minusResult >= 2) score = "Win for " + player1Name;
-            else score = "Win for " + player2Name;
+            score = getDeuceScore(player1Score);
+        } else if (player1Score > FORTY || player2Score > FORTY) {
+            score = getFinishScore(player1Name, player2Name, player1Score, player2Score);
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = player1Score;
-                else {
-                    score += "-";
-                    tempScore = player2Score;
-                }
-                switch (tempScore) {
-                    case love:
-                        score += "Love";
-                        break;
-                    case fifteen:
-                        score += "Fifteen";
-                        break;
-                    case thirty:
-                        score += "Thirty";
-                        break;
-                    case forty:
-                        score += "Forty";
-                        break;
-                }
-            }
+            score = setScore(player1Score, player2Score);
         }
         return score;
     }
 }
+
