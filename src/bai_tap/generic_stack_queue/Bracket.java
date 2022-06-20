5f87a96b7bfa175c470889586deacdfe5638f466
tranhoanglon98 <you@example.com>
2022-06-20 19:15:16 +0700

    update

--- src/bai_tap/generic_stack_queue/Bracket.java
+++ src/bai_tap/generic_stack_queue/Bracket.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Bracket {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a expression");
-        String expression = scanner.nextLine();
+    private static final char LEFT_BRACKET = '(';
+    private static final char RIGHT_BRACKET = ')';
+
+    public static boolean checkBracket(String expression) {
         Stack<Character> characterStack = new Stack<>();
         boolean isExpression = true;
         for (int i = 0; i < expression.length(); i++) {
-            if (expression.charAt(i) == '(') {
+            if (expression.charAt(i) == LEFT_BRACKET) {
                 characterStack.push(expression.charAt(i));
-            } else if (expression.charAt(i) == ')') {
+            } else if (expression.charAt(i) == RIGHT_BRACKET) {
                 if (characterStack.isEmpty()) {
                     isExpression = false;
                     break;
@@ -23,12 +23,14 @@ public class Bracket {
             }
         }
         if (!isExpression) {
-            System.out.println("false");
-        } else if (characterStack.isEmpty()) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+            return false;
+        } else return characterStack.isEmpty();
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a expression");
+        String expression = scanner.nextLine();
+        System.out.println(checkBracket(expression));
     }
 }
