a4c23905fa9b7aa31d432f0a9a569957ff2b376f
tranhoanglon98 <you@example.com>
2022-06-23 15:36:41 +0700

    upadat sort

--- src/bai_tap/sort_algorithm/task1.java
+++ src/bai_tap/sort_algorithm/task1.java
@@ -12,13 +12,14 @@ public class task1 {
             x = array[pos];
             System.out.println("chèn vị trí thích hợp cho phần tử: " + array[pos] + " index: " + pos);
             System.out.println(Arrays.toString(array));
+            System.out.println("gán x = " + array[pos]);
             while (pos > 0 && x < array[pos - 1]) {
-                System.out.println(array[pos - 1] + "-->" + array[pos]);
+                System.out.println(x + " < " + array[pos - 1] + " nên " + array[pos - 1] + "-->" + array[pos]);
                 array[pos] = array[pos - 1];
                 pos--;
                 System.out.println(Arrays.toString(array));
             }
-            System.out.println(x + "-->" + array[pos]);
+            System.out.println("x:" + x + "-->" + array[pos] + "(index: " + pos + ")");
             array[pos] = x;
             System.out.println(Arrays.toString(array));
         }
@@ -26,19 +27,19 @@ public class task1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter array size:");
+        System.out.println("Nhập chiều dài của mảng:");
         int size = scanner.nextInt();
         int[] array = new int[size];
         for (int i = 0; i < size; i++) {
-            System.out.println("Enter element at index + " + i);
+            System.out.println("Nhập phần tử tại vị trí: + " + i);
             array[i] = scanner.nextInt();
         }
-        System.out.println("array before sort");
+        System.out.println("Mảng trước khi sắp xếp");
         System.out.println(Arrays.toString(array));
         System.out.println("---------------------------------------------------");
         insertionSort(array);
         System.out.println("---------------------------------------------------");
-        System.out.println("array after sort");
+        System.out.println("Mảng sau khi sắp xếp");
         System.out.println(Arrays.toString(array));
     }
 }
